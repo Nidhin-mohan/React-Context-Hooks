f2a27cc300d8be81b4b5bfe44e22cee5d367bfbe
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-02-26 16:01:24 +0530

    added booklist using context

--- hooksapp/src/components/SongList.js
+++ hooksapp/src/components/SongList.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
+import NewSongForm from "./NewSongForm";
 
 const SongList = () => {
   const [songs, setSongs] = useState([
@@ -8,9 +9,15 @@ const SongList = () => {
     { title: "this wild darkness", id: 3 },
   ]);
   //console.log(songs);
-  const addSong = () => {
-    setSongs([...songs, { title: "new song", id: uuidv4() }]);
+  const addSong = (title) => {
+    setSongs([...songs, { title, id: uuidv4() }]);
   };
+
+    useEffect(() => {
+      console.log("useEffect callback", songs);
+    }, [songs]);
+
+    
   return (
     <div className="song-list">
       <ul>
@@ -18,7 +25,7 @@ const SongList = () => {
           return <li key={song.id}>{song.title}</li>;
         })}
       </ul>
-      <button onClick={addSong}>Add a song</button>
+      <NewSongForm addSong={addSong} />
     </div>
   );
 };
